a8e874e2394f790eb4ae8feee2f1e0cb5ce4de16
Dawid2806 <David-kaczmarek@gmx.de>
2022-08-18 20:59:01 +0200

    adding Countries box

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 import "./App.css";
-import { Countryitem } from "./Components/Countryitem/Countryitem";
+import { Countris } from "./Components/Country/Countris/Countris";
+import { Countryitem } from "./Components/Country/Countryitem/Countryitem";
 import { Filter } from "./Components/Filter/Filter";
 import { Header } from "./Components/Header/Header";
 import { SearchInput } from "./Components/Search/SearchInput";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="app">
-      <Header />
-      <div className="searchBox">
-        <SearchInput />
-        <Filter />
+    <QueryClientProvider client={queryClient}>
+      <div className="app">
+        <Header />
+        <div className="searchBox">
+          <SearchInput />
+          <Filter />
+        </div>
+        <>
+          <Countris />
+        </>
       </div>
-      <div className="container">
-        <Countryitem />
-        <Countryitem />
-        <Countryitem />
-        <Countryitem />
-        <Countryitem />
-        <Countryitem />
-        <Countryitem />
-        <Countryitem />
-      </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
